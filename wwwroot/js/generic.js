41b742ed1a6ac30eefe6ce80d17cdc988bf5b5ff
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2021-04-06 22:24:04 -0400

    imprimir funcionado

--- wwwroot/js/generic.js
+++ wwwroot/js/generic.js
@@ -11,3 +11,55 @@
             confirmButtonText: 'Si!'
     })
 }
+
+//funcion imprimir
+function Imprimir() {
+    //obtener el html que tiene el id tcheckbox
+    var tcheckbox = document.getElementById("tcheckbox").outerHTML;
+    //obtengo todo el contenido del la tabla
+    var tabla = "<h1>Reporte a Imprimir</h1>"
+    tabla += document.getElementById("tabla").outerHTML;
+    //elimino todo lo que tiene un id tcheckbox
+    tabla = tabla.replace(tcheckbox, "");
+
+    //todo el contenido de la www
+    var pagina = window.document.body;
+    var ventana = window.open();
+    //en la nueva venta agrego la tabla
+    ventana.document.write(tabla);
+    ventana.print();
+    ventana.close();
+    //devuelta a la pagina
+    window.document.body = pagina;
+
+}
+
+// funcion table.js
+window.onload = function () {
+    $(document).ready(function () {
+        $('#tabla').DataTable();
+    });
+
+}
+
+
+//funcion para exportar excel
+function ExportarExcel() {
+    document.getElementById("tipoReporte").value = "Excel";
+    var frmReporte = document.getElementById("frmReporte");
+    frmReporte.submit();
+}
+
+//funcion para exportar word
+function ExportarWord() {
+    document.getElementById("tipoReporte").value = "Word";
+    var frmReporte = document.getElementById("frmReporte");
+    frmReporte.submit();
+}
+
+//funcion para exportar Pdf
+function ExportarPdf() {
+    document.getElementById("tipoReporte").value = "PDF";
+    var frmReporte = document.getElementById("frmReporte");
+    frmReporte.submit();
+}
